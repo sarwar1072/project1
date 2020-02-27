f8f89eb2ea5539e6d957466e6d61725acb82d51a
sarwar mahmud milon <sarwar.cse.diu@gmail.com>
2020-02-28 00:00:25 +0600

    modified assignment

---
+++ AdminTheme.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdminTheme.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
